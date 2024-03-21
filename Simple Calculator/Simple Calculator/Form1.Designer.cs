03c94434febb039ae0fe5ce4a554fe70f0c285b2
harunzsln <harunzsln0@gmail.com>
2024-03-22 00:31:51 +0300

    Simple Calculator app
    Simple calculator with windows form app

---
+++ Simple Calculator/Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblResultLabel = new System.Windows.Forms.Label();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.lblSecondNumberLabel = new System.Windows.Forms.Label();
+            this.lblFirstNumberLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Controls.Add(this.lblResultLabel);
+            this.groupBox1.Controls.Add(this.txtSecondNumber);
+            this.groupBox1.Controls.Add(this.txtFirstNumber);
+            this.groupBox1.Controls.Add(this.lblSecondNumberLabel);
+            this.groupBox1.Controls.Add(this.lblFirstNumberLabel);
+            this.groupBox1.Location = new System.Drawing.Point(19, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 176);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(34, 136);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 16);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Result";
+            // 
+            // lblResultLabel
+            // 
+            this.lblResultLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.lblResultLabel.Location = new System.Drawing.Point(252, 136);
+            this.lblResultLabel.Name = "lblResultLabel";
+            this.lblResultLabel.Size = new System.Drawing.Size(110, 16);
+            this.lblResultLabel.TabIndex = 6;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(255, 91);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(110, 22);
+            this.txtSecondNumber.TabIndex = 4;
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(255, 46);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(110, 22);
+            this.txtFirstNumber.TabIndex = 3;
+            // 
+            // lblSecondNumberLabel
+            // 
+            this.lblSecondNumberLabel.AutoSize = true;
+            this.lblSecondNumberLabel.Location = new System.Drawing.Point(34, 93);
+            this.lblSecondNumberLabel.Name = "lblSecondNumberLabel";
+            this.lblSecondNumberLabel.Size = new System.Drawing.Size(105, 16);
+            this.lblSecondNumberLabel.TabIndex = 1;
+            this.lblSecondNumberLabel.Text = "Second Number";
+            // 
+            // lblFirstNumberLabel
+            // 
+            this.lblFirstNumberLabel.AutoSize = true;
+            this.lblFirstNumberLabel.Location = new System.Drawing.Point(34, 49);
+            this.lblFirstNumberLabel.Name = "lblFirstNumberLabel";
+            this.lblFirstNumberLabel.Size = new System.Drawing.Size(83, 16);
+            this.lblFirstNumberLabel.TabIndex = 0;
+            this.lblFirstNumberLabel.Text = "First Number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDivide);
+            this.groupBox2.Controls.Add(this.btnMultiply);
+            this.groupBox2.Controls.Add(this.btnSubstract);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Location = new System.Drawing.Point(19, 213);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(439, 112);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(309, 46);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(96, 29);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(213, 46);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(96, 29);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(117, 46);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(96, 29);
+            this.btnSubstract.TabIndex = 1;
+            this.btnSubstract.Text = "Substract";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(21, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(96, 29);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 491);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumberLabel;
+        private System.Windows.Forms.Label lblFirstNumberLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblResultLabel;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Simple Calculator/Simple Calculator/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Calculator
+{
+    public partial class Form1 : Form
+    {
+        double firstNumber;
+        double secondNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            try
+                {
+                    firstNumber = Convert.ToInt32(txtFirstNumber.Text);
+                }
+            catch { }
+
+                try
+                {
+                    secondNumber = Convert.ToInt32(txtSecondNumber.Text);
+                }
+                catch { }
+
+
+                lblResultLabel.Text = (firstNumber + secondNumber).ToString();
+            }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                firstNumber = Convert.ToInt32(txtFirstNumber.Text);
+            }
+            catch { }
+
+            try
+            {
+                secondNumber = Convert.ToInt32(txtSecondNumber.Text);
+            }
+            catch { }
+
+
+            lblResultLabel.Text = (firstNumber - secondNumber).ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                firstNumber = Convert.ToInt32(txtFirstNumber.Text);
+            }
+            catch { }
+
+            try
+            {
+                secondNumber = Convert.ToInt32(txtSecondNumber.Text);
+            }
+            catch { }
+
+
+            lblResultLabel.Text = (firstNumber * secondNumber).ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                firstNumber = Convert.ToInt32(txtFirstNumber.Text);
+            }
+            catch { }
+
+            try
+            {
+                secondNumber = Convert.ToInt32(txtSecondNumber.Text);
+            }
+            catch { }
+
+            if (firstNumber == 0 && secondNumber == 0)
+            {
+                MessageBox.Show("Result is undetermined");
+                
+            }
+
+            if (secondNumber == 0)
+            {
+                MessageBox.Show("Result is undefined");
+                return;
+
+            }
+            else
+            {
+                lblResultLabel.Text = (firstNumber / secondNumber).ToString();
+            }
+            
+        }
+
+    }
+    
+}
